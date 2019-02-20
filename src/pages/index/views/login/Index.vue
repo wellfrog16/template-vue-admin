ee33d09ccc6e785a2ebd580c4d351b9528b9cd3b
wellfrog <wellfrog16@gmail.com>
2019-02-20 18:09:50 +0800

    增加d登陆页面

--- src/pages/index/views/login/Index.vue
+++ src/pages/index/views/login/Index.vue
@@ -1,5 +1,14 @@
 <template>
-    <div>
-        登陆
+    <div :class="$style.main">
+        登陆1234
     </div>
 </template>
+
+<style lang="less" module>
+@import '../../../../assets/style/config.less';
+
+.main {
+    background-color: @g-color-primary;
+    color: #fff;
+}
+</style>
