da58a0da8a494b82ba504f290983a1dca288c8b5
wellfrog <wellfrog16@gmail.com>
2019-02-20 16:32:32 +0800

    修复触摸指令

--- src/utils/directive.js
+++ src/utils/directive.js
@@ -1,43 +1,49 @@
 import Vue from 'vue';
 import { Hammer } from '@/utils/cdn';
 
-function vueTouch(el, type, binding) {
-    const hammertime = new Hammer(el);
-    hammertime.on(type, binding.value);
+function vueTouch(el, binding, re, ev = re) {
+    const hammertime = new Hammer.Manager(el);
+    const recognizers = {
+        tap: Hammer.Tap,
+        press: Hammer.Press,
+        swipe: Hammer.Swipe,
+    };
+    hammertime.add(new recognizers[re]());
+    hammertime.on(ev, binding.value);
 }
 
 Vue.directive('tap', {
     bind(el, binding) {
-        vueTouch(el, 'tap', binding);
+        vueTouch(el, binding, 'tap');
     },
 });
 
 Vue.directive('swipeleft', {
     bind(el, binding) {
-        vueTouch(el, 'swipeleft', binding);
+        vueTouch(el, binding, 'swipe', 'swipeleft');
     },
 });
 
 Vue.directive('swiperight', {
     bind(el, binding) {
-        vueTouch(el, 'swiperight', binding);
+        vueTouch(el, binding, 'swipe', 'swiperight');
     },
 });
 
 Vue.directive('swipeup', {
     bind(el, binding) {
-        vueTouch(el, 'swipeup', binding);
+        vueTouch(el, binding, 'swipe', 'swipeup');
     },
 });
 
 Vue.directive('swipedown', {
     bind(el, binding) {
-        vueTouch(el, 'swipedown', binding);
+        vueTouch(el, binding, 'swipe', 'swipedown');
     },
 });
 
 Vue.directive('press', {
     bind(el, binding) {
-        vueTouch(el, 'press', binding);
+        vueTouch(el, binding, 'press');
     },
 });
