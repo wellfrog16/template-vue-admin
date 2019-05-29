fd183acf3224b6e8ed80f32154042ca971056453
wellfrog <wellfrog16@gmail.com>
2019-05-30 02:19:49 +0800

    修复菜单bug

--- src/pages/index/components/common/menu/index.vue
+++ src/pages/index/components/common/menu/index.vue
@@ -45,7 +45,7 @@ export default {
         isHiddenChildren(item) {
             let isHidden = false;
             if (item.children && item.children.length > 0) {
-                isHidden = item.children.some(item1 => item1.meta && item1.meta.hidden);
+                isHidden = item.children.every(item1 => item1.meta && item1.meta.hidden);
             }
             return isHidden;
         },
