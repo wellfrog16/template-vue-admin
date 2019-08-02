0c60efa257c3a9d7f157094ee147856741e5db76
frog <wellfrog16@gmail.com>
2019-08-02 18:03:16 +0800

    调试全局通知

--- src/pages/index/components/layout/notification/index.vue
+++ src/pages/index/components/layout/notification/index.vue
@@ -5,24 +5,38 @@
 import { mapState, mapMutations } from 'vuex';
 
 export default {
+    data() {
+        return {
+            messageDuration: 3000,
+            notificationDuration: 4500,
+        };
+    },
     computed: {
         ...mapState(['message', 'notification']),
     },
     watch: {
-        message(val) {
-            val && this.showMessage(val);
+        message(val1, val2) {
+            if (val1 && val2) {
+                if (val1.type !== val2.type || val1.message !== val2.message) {
+                    this.showMessage(val1);
+                }
+            }
         },
 
         notification(val) {
             val && this.showNotification(val);
         },
+
+        // todo同样的消息同时只出现一次
     },
     methods: {
         ...mapMutations(['clearMessage', 'clearNotification']),
 
         showMessage(options) {
+            options.duration = options.duration || this.messageDuration;
+            const { duration } = options;
             this.$message(options);
-            this.clearMessage();
+            setTimeout(() => this.clearMessage(), duration);
         },
 
         showNotification(options) {
