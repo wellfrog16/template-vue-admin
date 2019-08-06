b87ed23fdfe2fc45b75991b760dd26dad425f3a7
wellfrog <wellfrog16@gmail.com>
2019-08-07 02:41:31 +0800

    增加全局挂载

--- src/pages/index/main.js
+++ src/pages/index/main.js
@@ -3,6 +3,8 @@ import store from './store';
 import router from './helper/router';
 import App from './App.vue';
 import { helper } from '@/helper/lakes';
+import { utils } from '@/utils/rivers';
+import cdn from '@/utils/cdn';
 
 // 过滤器
 import '@/utils/filters';
@@ -15,8 +17,13 @@ import '@/plugins';
 
 Vue.config.productionTip = false;
 
-helper.vue = new Vue({
+window.vueIndex = new Vue({
     router,
     store,
     render: h => h(App),
+    created() {
+        this.$helper = helper;
+        this.$utils = utils;
+        this.$cdn = cdn;
+    },
 }).$mount('#app');
