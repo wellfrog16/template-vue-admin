7de8c35956a1ab5d9ae622ebb9b141543733fa0a
frog <wellfrog16@gmail.com>
2019-09-02 11:15:10 +0800

    更新eslint规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
         'window': true,
     },
     rules: {
-        indent: [2, 4], // 缩进风格
+        indent: [2, 4, { "SwitchCase": 1 }], // 缩进风格
         'max-len': [2, { 'code': 150 }],
         'no-underscore-dangle': [2, { 'allow': ['_id'] }], // nedb专用
         'no-unused-expressions': [2, { 'allowShortCircuit': true, 'allowTernary': true }], // ? 允许 a && a()写法
