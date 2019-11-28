e2e39b186bece8a0261a54b9bf853be0f6ed3f9a
frog <wellfrog16@gmail.com>
2019-11-28 18:12:06 +0800

    增加base64和file互转

--- src/utils/index.js
+++ src/utils/index.js
@@ -233,6 +233,34 @@ function deepMerge(target, obj) {
     _.mergeWith(target, obj, customizer);
 }
 
+/**
+ * file格式转base64
+ *
+ * @param {*} file
+ * @returns
+ */
+function file2base64(file) {
+    return new Promise((resolve) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+    });
+}
+
+// 将base64转换为文件
+function base642file(dataurl, filename) {
+    const arr = dataurl.split(',');
+    const mime = arr[0].match(/:(.*?);/)[1];
+    const bstr = atob(arr[1]);
+    let n = bstr.length;
+    const u8arr = new Uint8Array(n);
+    while (n) {
+        n -= 1;
+        u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new File([u8arr], filename, { type: mime });
+}
+
 export default {
     isEmpty,
     getUrlParam,
@@ -245,4 +273,6 @@ export default {
     deepMerge,
     delay,
     deleteHtmlTag,
+    file2base64,
+    base642file,
 };
