f39f938871b09114639d54b7c47fddf40c83ef6a
frog <wellfrog16@gmail.com>
2020-03-11 14:49:59 +0800

    clickoutside改为调用element的

--- src/directive/outside-click/index.js
+++ src/directive/outside-click/index.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
-import outsideClick from './outside-click';
+// import outsideClick from './outside-click';
+import Clickoutside from 'element-ui/src/utils/clickoutside';
 
-Vue.directive('outside-click', outsideClick);
+Vue.directive('outside-click', Clickoutside);
