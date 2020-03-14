21c433490d45ac34008322a29f106d4168f77cff
wellfrog <wellfrog16@gmail.com>
2020-03-15 00:21:05 +0800

    增加三角形less函数

--- src/pages/index/views/test/Index.vue
+++ src/pages/index/views/test/Index.vue
@@ -1,17 +1,28 @@
 <template>
     <el-card shadow="never">
         测试
+        <div :class="$style.test"></div>
     </el-card>
 </template>
 
 <script>
-import api from '@/api/sys/upload';
+// import api from '@/api/sys/upload';
 
 export default {
     mounted() {
-        console.log(File);
-        api();
+        // console.log(File);
+        // api();
         // api.test2({ code: '999' });
     },
 };
 </script>
+
+<style lang="less" module>
+@import '~@/assets/style/common/function.less';
+
+.test {
+    .triangle(right, 10px, red);
+
+    line-height: 1;
+}
+</style>
